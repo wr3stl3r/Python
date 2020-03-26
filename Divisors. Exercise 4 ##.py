a3df8818684373d3dd8dbbeeb67e3561c126d2cd
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-03-26 19:46:41 +0200

    Update Divisors. Exercise 4 ##.py

--- Divisors. Exercise 4 ##.py
+++ Divisors. Exercise 4 ##.py
@@ -5,14 +5,14 @@ print("Your enered number is", str(len(find_devisors)), "digits long.")
 print(".................................")
 print("Your", find_devisors, "devisors are: \n")
 
-def WTF():
+def the_function():
 	if int(find_devisors) % i == 0:
 		print(i)
 
 i = 0
 for i in range(int(find_devisors)):
 	i = i+1
-	WTF()
+	the_function()
 
 
 print("\nDone!\nThank you for using our devisor finder :)")
