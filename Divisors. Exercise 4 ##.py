f66271a5a984944b0d2f1603b8b0803bc946b3e6
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-03-26 19:45:22 +0200

    Add files via upload

---
+++ Divisors. Exercise 4 ##.py
@@ -0,0 +1,19 @@
+find_devisors = input("Hello there enter number and i will...\n \
+find devisors for your number: ")
+print()
+print("Your enered number is", str(len(find_devisors)), "digits long.") 
+print(".................................")
+print("Your", find_devisors, "devisors are: \n")
+
+def WTF():
+	if int(find_devisors) % i == 0:
+		print(i)
+
+i = 0
+for i in range(int(find_devisors)):
+	i = i+1
+	WTF()
+
+
+print("\nDone!\nThank you for using our devisor finder :)")
+
