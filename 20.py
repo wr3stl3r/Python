92d2594410b4f592b7eb0616f6456afa7dc7d435
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-12-07 19:40:06 +0200

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,52 +1,51 @@
 # https://www.practicepython.org/exercise/2014/11/11/20-element-search.html
 # Element Search
-# Len is no good as index
-# Cuz what do you do when search
-# NR in other half of list!
 
+# Function with our Binary search
 def inside(List, user_input):
     if user_input in List:
         return True;
     else:
         return False;
-        
-def midIndx(X):
+ 
+# Here starts Binary search        
+def midIndx(X): # Gets mid index for all time!
     res = int(len(X) // 2);
     return res;
 
-
-
 def binarySearch(List,user_input):
-    # 7 [1,2,3,4,5,6,7,8,9,10,11]
-    list_even = True if len(List) % 2 == 0 else False;
-    list_odd = True if len(List) % 2 == 1 else False;
-    mid_inx = List[midIndx(List)];
+    bin_search_steps = 1; # Counts how many time List is split in half before result is found
+    mid_number = List[midIndx(List)]; # Number in ~middle of List
     # List 
     while True:
-        if user_input == mid_inx:
-            return "YES you found it"
-        elif user_input < mid_inx:
-            List = List[?:midIndx(List)];
-            mid_inx = List[midIndx(List)];
-    
-    
-    
-    
-    
-    
-    
-    
+        # ~ print(List);
+        if user_input == mid_number:
+            return f"YES you found {UI}. It's in the List. It took {bin_search_steps} Binary Search steps!"
+            break;
+        elif len(List)==1:
+            if user_input not in List:
+                return f"Sorry {UI} is not in the List. It took {bin_search_steps} Binary Search steps!"; 
+        elif user_input < mid_number:
+            List = List[:midIndx(List)];
+            mid_number = List[midIndx(List)];
+        elif user_input > mid_number:
+            List = List[midIndx(List):];
+            mid_number = List[midIndx(List)];   
+        bin_search_steps += 1;
+"""List [1-50]"""
+# ~ L = [i for i in range(1,51)]; 
+"""List [1-10000000]"""
+# ~ L = [i for i in range(1,10**7+1)]
 
+L= [1,2,3,5,6,8,9,10,11,15,17,20,22,25,27,30,100] # Current List
 
+# ~ print(inside(L,UI)); #search NOT using binary seach
 
+"""Optional checks, List, Len, midle index"""
+# ~ print(f"Len of list = {len(L)}");
+# ~ print(f"The List ==...\n{L}");
+# ~ print(f" mid Indx {midIndx(L)}");
 
-
-L = [i for i in range(1,52)];
+"""Call Binary seach function"""
 UI = int(input("Hi! Is your number in list? Type here: "));
-# ~ print(inside(L,UI));
-
-print(len(L));
-print(L);
-print(f" mid Indx {midIndx(L)}");
 print(binarySearch(L,UI));
-
